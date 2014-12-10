ca7ea315234a5201f08d2e1955521f672a91fa2f
yiqin <yiqin.mems@gmail.com>
2014-12-10 14:15:49 -0500

    Update

---
+++ create_new_account.php
@@ -0,0 +1,45 @@
+<?php
+
+
+// Connection parameters 
+$host = 'cspp53001.cs.uchicago.edu';
+$username = 'yiqin';
+$password = 'aitiejah';
+$database = $username.'DB';
+
+// Attempting to connect
+$dbcon = mysqli_connect($host, $username, $password, $database)
+   or die('Could not connect: ' . mysqli_connect_error());
+// print 'Connected successfully!<br>';
+
+// Getting the input parameter (user):
+$user = $_REQUEST['user'];
+$timezone = $_REQUEST['timezone'];
+$language = $_REQUEST['language'];
+$email = $_REQUEST['email'];
+
+
+// Get the attributes of the user with the given username
+$query = "INSERT INTO User(name, timezone, language, email)
+		  VALUES('$user', '$timezone', '$language', '$email')";
+
+$result = mysqli_query($dbcon, $query);
+if (!$result) {
+    die('Failed: ' . mysqli_error($dbcon));
+}
+else {
+	header('Location: /tbp.html');
+}
+
+
+
+// Free result
+mysqli_free_result($result);
+
+// Closing connection
+mysqli_close($dbcon);
+
+
+
+
+?> 
